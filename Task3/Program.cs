bbd9c51885b82f7b65a0208a3db8dc068dbf4a54
Listratova Tatyana <listratova1@gmail.com>
2022-09-14 15:44:13 +0300

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// программа принимает на вход массив из 8 элементов и выводит их на экран
+
+int[] GetArray()
+{
+    int[] myArray = new int [8];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i]= int.Parse(Console.ReadLine());
+    }
+    return myArray;
+}
+
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+			
+}
+
+Console.WriteLine("Введите 8 элементов массива: ");
+int [] myArrayInput = GetArray();
+
+PrintArray(myArrayInput);
+
+Console.ReadLine();
+
